e2eb33c99b0b6f7be46824350f06cc7fecdcd3b5
Stefan Johnson <sj@scrimpycat.io>
2017-11-26 04:21:45 +1100

    Setup package

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SimpleMarkdownExtensionCLI
-An extension for SimpleMarkdown to add renderer for CLI output
+An extension for SimpleMarkdown to add renderer for CLI output.
+
+Installation
+------------
+
+```elixir
+def applications do
+    [applications: [:simple_markdown_extension_cli]]
+end
+
+def deps do
+    [{ :simple_markdown_extension_cli, "~> 0.0.1" }]
+end
+```
--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule SimpleMarkdownExtensionCLI.Mixfile do
     def project do
         [
             app: :simple_markdown_extension_cli,
+            description: "An extension for SimpleMarkdown to add renderer for CLI output.",
             version: "0.0.1",
             elixir: "~> 1.5",
             start_permanent: Mix.env == :prod,
-            deps: deps()
+            deps: deps(),
+            package: package()
         ]
     end
 
@@ -19,5 +21,13 @@ defmodule SimpleMarkdownExtensionCLI.Mixfile do
     # Run "mix help deps" to learn about dependencies.
     defp deps do
         [{ :simple_markdown, "~> 0.3.0" }]
+    end
+
+    defp package do
+        [
+            maintainers: ["Stefan Johnson"],
+            licenses: ["BSD 2-Clause"],
+            links: %{ "GitHub" => "https://github.com/ScrimpyCat/SimpleMarkdownExtensionCLI" }
+        ]
     end
 end
